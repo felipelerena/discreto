2b26c02056d706ae941b66b9b4c5f69f671fc149
Felipe Lerena <felipelerena@gmail.com>
2018-03-16 22:08:44 -0300

    removing images dir by default

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+mkdir images -p
 docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v $(pwd)/images:/home/user/images -v $(pwd)/scripts:/home/user/scripts discreto
